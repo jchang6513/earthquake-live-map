d8474afc5b0c0e98bd12499c846d2bc14f974784
Yi-Wei Chang <jchang6513@gmail.com>
2019-06-23 14:18:43 +0800

    shrink code

--- src/presenters/EarthquakesMapPresenter.tsx
+++ src/presenters/EarthquakesMapPresenter.tsx
@@ -14,38 +14,15 @@ export default class EarthquakesMapPresenter implements IEarthquakeMapPresneter 
 
   validateRetangle(rect: Rectangle): validateMessage {
     const { minLat, maxLat, minLong, maxLong } = rect;
-    if (minLat >= maxLat) {
-      return {
-        type: 'warning',
-        content: 'Range of selected latitude is incorrect'
-      };
-    }
-
-    if (minLong >= maxLong) {
-      return {
-        type: 'warning',
-        content: 'Range of selected longitude is incorrect'
-      };
-    }
-
-    if (minLat < -90 || maxLat > 90) {
-      return {
-        type: 'success',
-        content: 'Selected latitude is out of range'
-      }
-    }
-
-    if (minLong < -180 || maxLong > 180) {
-      return {
-        type: 'success',
-        content: 'Selected longitude is out of range'
-      }
-    }
-
-    return {
-      type: 'success',
-      content: ''
-    };
+    if (minLat >= maxLat)
+      { return { type: 'warning', content: 'Range of selected latitude is incorrect' }; }
+    if (minLong >= maxLong)
+      { return { type: 'warning', content: 'Range of selected longitude is incorrect' }; }
+    if (minLat < -90 || maxLat > 90)
+      { return { type: 'success', content: 'Selected latitude is out of range' } }
+    if (minLong < -180 || maxLong > 180)
+      { return { type: 'success', content: 'Selected longitude is out of range' } }
+    return { type: 'success', content: '' };
   }
 
   validateCircle(circle: Circle): boolean {
